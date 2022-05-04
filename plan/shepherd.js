04d3828d83c9e7dddac6de3a84e1ac5299865cc0
Joshua Dance <joshua.dance@gmail.com>
2022-05-04 17:03:15 -0600

    firebase is working. I feel powerful

--- plan/shepherd.js
+++ plan/shepherd.js
@@ -1,6 +1,77 @@
 console.log(firebaseApp);
 const shepherdGreeting = 'Hello. I am Shepherd.';
 
+const auth = firebaseApp.auth();
+const db = firebaseApp.firestore();
+
+const whenSignedInSection = document.getElementById('whenSignedInSection');
+const whenSignedOutSection = document.getElementById('whenSignedOutSection');
+
+const signInWithGoogleButton = document.getElementById('signInWithGoogleButton');
+const signOutButton = document.getElementById('signOutButton');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInWithGoogleButton.onclick = () => auth.signInWithPopup(provider);
+
+signOutButton.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(updateUiForUserState);
+
+let userMessageH3 = document.createElement("h3");
+userDetails.appendChild(userMessageH3);
+
+//db section
+console.log(db);
+
+const planDetailsSection = document.getElementById('planDetailsSection');
+
+
+let plansReference;
+let unsubscribe;
+//end db section
+
+function updateUiForUserState(user){
+    if (user) {
+        //signed in
+        whenSignedInSection.hidden = false;
+        whenSignedOutSection.hidden = true;
+
+        userMessageH3.innerHTML = 'Hello ' + user.displayName;
+
+        plansReference = db.collection('plans');
+
+        plansReference.where('userId','==', user.uid).
+        get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach(doc => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+                updatePlan(doc);
+            });
+        })
+        .catch((error) => {
+            console.log('Error gettings documents: ' + error);
+        });
+        
+    } else {
+        //signed out
+        whenSignedInSection.hidden = true;
+        whenSignedOutSection.hidden = false;
+    }
+}
+
+function updatePlan(doc) {
+    const planLength = document.getElementById('planLength');
+    planLength.innerHTML = 'Length: ' + doc.data().lengthInDays;
+
+    const bodyFatChangeSection = document.getElementById('bodyFatChangeSection');
+    bodyFatChangeSection.innerHTML = 'Staring body fat ' + doc.data().planStartingBodyFatPercentage
+    + '% - Ending body fat ' + doc.data().planEndingBodyFatPercentage + '%.';
+}
+
 document.addEventListener("DOMContentLoaded", startUpTheCalculator);
 function startUpTheCalculator() {
     console.log(shepherdGreeting);
