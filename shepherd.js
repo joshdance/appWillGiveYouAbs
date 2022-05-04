b6d26dbae40b4ccc0a63b2d90c14f72d58a27271
Joshua Dance <joshua.dance@gmail.com>
2022-05-04 08:41:27 -0600

    started adding in firebase

--- shepherd.js
+++ shepherd.js
@@ -1,4 +1,7 @@
+console.log(firebaseApp);
+const shepherdGreeting = 'Hello. I am Shepherd.';
+
 document.addEventListener("DOMContentLoaded", startUpTheCalculator);
 function startUpTheCalculator() {
-    console.log('Hello. I am Shepherd.');
+    console.log(shepherdGreeting);
 }
