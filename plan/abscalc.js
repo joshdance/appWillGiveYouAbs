fcfb0e839caeb0f2882a8e34d342a1d032e27443
Joshua Dance <joshua.dance@gmail.com>
2022-08-29 14:38:28 -0600

    finally got everything working how I want it.

--- plan/abscalc.js
+++ plan/abscalc.js
@@ -35,31 +35,10 @@ if (navCheckbox != null) {
     navCheckbox.addEventListener('click', navHamburgerClicked);
 }
 
+let navBarLinks = document.getElementById('navBarLinks');
+
 function navHamburgerClicked(event) {
-    if (navCheckbox.checked == true) {
-        console.log('show it');
-        let menuItemsToShow = document.getElementsByClassName('menuItem');
-        Array.from(menuItemsToShow).forEach((element) => {
-            element.style.display = 'block';
-        });
-
-        let closeSVG = document.getElementById('closeSVG');
-        closeSVG.style.display = 'block';
-
-        let menuSVG = document.getElementById('menuSVG');
-        menuSVG.style.display = 'none';
-    } else {
-        let menuItemsToShow = document.getElementsByClassName('menuItem');
-        Array.from(menuItemsToShow).forEach((element) => {
-            element.style.display = 'none';
-        });
-
-        let closeSVG = document.getElementById('closeSVG');
-        closeSVG.style.display = 'none';
-
-        let menuSVG = document.getElementById('menuSVG');
-        menuSVG.style.display = 'block';
-    }
+    navBarLinks.classList.toggle('active');
 }
 
 
